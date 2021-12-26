2ded194edd2542acd6c65e743089ff767ec4a06b
sdoshi983 <sdoshi983@rku.ac.in>
2021-12-27 00:07:09 +0530

    card component created

--- src/App.js
+++ src/App.js
@@ -18,12 +18,12 @@ class App extends Component{
   render(){
     return (
       <div className="App">
-        <CardList sd="sdd"/>
-        {
+        <CardList>{
           this.state.monsters.map(monster => (
             <h1 key = {monster.id}>{monster.name}</h1>
           ))
-        }
+        }</CardList>
+        
       </div>
     )
   }
