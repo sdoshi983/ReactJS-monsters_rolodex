5f7d3228e37ed221e8197bbcfb556b0b97e4520a
sdoshi983 <sdoshi983@rku.ac.in>
2021-12-27 17:00:04 +0530

    robohash api used for profile images

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import './card-list.styles.css'
+import './card-list.styles.css';
+import {Card} from '../card/card.component.jsx';
 
-export const CardList = (props) => {
-    console.log(props);
-    return <div className='card-list'>
-        {
-          props.monsters.map(monster => (
-            <h1 key = {monster.id}>{monster.name}</h1>
-          ))
-        }
-    </div>;
-}
+export const CardList = (props) => (
+    <div className='card-list'>
+    {
+      props.monsters.map(monster => (
+        <Card key = {monster.id} monster = {monster} />
+      ))
+    }
+    </div>
+)
